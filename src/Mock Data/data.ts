4283f9c21a7d336b50cb20144ffbf17dfe874085
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-05-07 17:18:12 +0530

    added mock data to display

---
+++ src/Mock Data/data.ts
@@ -0,0 +1,92 @@
+export let data = [
+  {
+    id: 1,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 2,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 3,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 4,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 5,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 6,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 7,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 8,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 9,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+  {
+    id: 10,
+    img: "https://randomuser.me/api/portraits/women/66.jpg",
+    name: "MrBeast",
+    verfied: false,
+    email: "test@example.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Tortor ut cras mauris at faucibus pharetra pellentesque diam pulvinar. Mauris penatibus ut luctus posuere posuere odio nisi mauris aliquet. Sapien aliquet porta tincidunt massa id quam pharetra. Massa vitae feugiat vulputate et praesent nisl neque nunc tortor.",
+  },
+];
