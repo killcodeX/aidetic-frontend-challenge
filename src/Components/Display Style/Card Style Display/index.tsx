99ce56416bef97bf799a3256cc671835ffb53af4
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-05-07 19:35:54 +0530

    list display done

--- src/Components/Display Style/Card Style Display/index.tsx
+++ src/Components/Display Style/Card Style Display/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React from "react";
+import { data } from "../../../Mock Data/data";
 
 export default function CardDisplay() {
   return (
-    <div>CardDisplay</div>
-  )
+    <div className="row">
+      {data.map((item) => {
+        return (
+          <div key={item.id} className="col-sm-3 mb-5">
+            <div className="card">
+              <div className="card-upper">
+                <div className="profile">
+                  <img src={item.img} alt={item.name} />
+                </div>
+                <div className="profile-details">
+                  <div className="d-flex flex-row align-items-center">
+                    <div className="profile-name px-1">{item.name}</div>
+                    {!item.verfied ? (
+                      <img
+                        className="verified-badge"
+                        src={process.env.PUBLIC_URL + "/verified.png"}
+                        alt="verified"
+                      />
+                    ) : null}
+                  </div>
+                  <div className="profile-email">{item.email}</div>
+                </div>
+                <div className="profile-edit">
+                  <i className="ri-more-2-fill"></i>
+                </div>
+              </div>
+              <div className="card-lower">{item.description}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/Components/Display Style/List Style Display/index.tsx
+++ src/Components/Display Style/List Style Display/index.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React from "react";
+import { data } from "../../../Mock Data/data";
 
 export default function ListDisplay() {
   return (
-    <div>ListDisplay</div>
-  )
+    <div className="row">
+      <div className="col">
+        <table className="list-display">
+          <thead>
+            <tr>
+              <th style={{ width: "15%" }}>Name</th>
+              <th style={{ width: "10%" }}>ID</th>
+              <th style={{ width: "15%" }}>Email</th>
+              <th style={{ width: "50%" }}>Description</th>
+              <th style={{ width: "10%", textAlign: "right" }}>
+                <i className="ri-settings-3-fill"></i>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => {
+              return (
+                <tr className="mb-4" key={item.id}>
+                  <td>
+                    <div className="list-profile-details">
+                      <div className="profile">
+                        <img src={item.img} alt={item.name} />
+                      </div>
+                      <div className="list-profile-name">{item.name}</div>
+                      {!item.verfied ? (
+                        <img
+                          className="verified-badge"
+                          src={process.env.PUBLIC_URL + "/verified.png"}
+                          alt="verified"
+                        />
+                      ) : null}
+                    </div>
+                  </td>
+                  <td>
+                    <div className="list-profile-id">{item.id}</div>
+                  </td>
+                  <td>
+                    <div className="list-profile-email">{item.email}</div>
+                  </td>
+                  <td>
+                    <div className="list-profile-description">
+                      {item.description}
+                    </div>
+                  </td>
+                  <td style={{ textAlign: "right" }}>
+                    <div className="list-profile-action">
+                      <i className="ri-more-2-fill"></i>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
