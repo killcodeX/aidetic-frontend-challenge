3091763cdf41d98247ca79dff86b14bdcf7a3e0e
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-05-07 14:56:45 +0530

    theme icon updated

--- src/Components/Header/theme.tsx
+++ src/Components/Header/theme.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { Switch } from "antd";
 
-interface themeProps{
+interface themeProps {
   theme: boolean;
   setTheme: (value: boolean) => void;
 }
 
-export default function Theme({theme, setTheme}:themeProps) {
+export default function Theme({ theme, setTheme }: themeProps) {
   useEffect(() => {
     if (!theme) {
       document.documentElement.setAttribute("data-theme", "light");
@@ -17,12 +17,20 @@ export default function Theme({theme, setTheme}:themeProps) {
 
   return (
     <div className="theme-change">
-      <span className={`theme ${!theme ? "active" : ""}`}>
-        <i className="ri-sun-line"></i>
+      <span className="theme">
+      {!theme ? (
+          <i className="ri-sun-fill"></i>
+        ) : (
+          <i className="ri-sun-line"></i>
+        )}
       </span>
       <Switch checked={theme} onChange={() => setTheme(!theme)} />
-      <span className={`theme ${theme ? "active" : ""}`}>
-        <i className="ri-moon-line"></i>
+      <span className="theme">
+        {theme ? (
+          <i className="ri-moon-fill"></i>
+        ) : (
+          <i className="ri-moon-line"></i>
+        )}
       </span>
     </div>
   );
